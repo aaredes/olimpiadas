0bf2daa164636f1a9d5fcd3954a8b80eaa050797
aaredes <113741217+aaredes@users.noreply.github.com>
2024-09-09 00:39:00 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+      titulo: "Rayssa Leal",
+      descricao: "Rayssa Leal, conhecida como \"Fadinha\", é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela se tornou a mais jovem medalhista olímpica da história do Brasil, trazendo ainda mais visibilidade para o esporte feminino.",
+      link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+      tags: "skate fada sk8"
+    },
+    {
+      titulo: "Beatriz Souza",
+      descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nas Olimpíadas de Paris. Sua conquista histórica representou um marco para o esporte feminino brasileiro e para a comunidade negra.",
+      link: "https://pt.wikipedia.org/wiki/Beatriz_Souza", // Substitua pelo link correto da Beatriz Souza
+      tags: "judô judo judoka"
+    },
+    {
+      titulo: "Rebeca Andrade",
+      descricao: "Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros e se tornou um dos maiores nomes do esporte nacional. Com sua força, determinação e talento inegável, Rebeca já coleciona diversas medalhas em competições nacionais e internacionais, incluindo o ouro olímpico.",
+      link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+      tags: "ginástica ginastica ouro"
+    }
+  ];
+    
